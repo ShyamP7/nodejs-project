24407a1e7b95301d01e1a2c3d224181da70420b9
shyam <shyamprasadp907@gmail.com>
2022-11-03 11:47:30 +0530

    ejs template engine added

--- app.js
+++ app.js
@@ -2,22 +2,26 @@
 const path = require('path');
 const express = require('express');
 const bodyparser = require('body-parser');
-const rootdir = require('../03-finished/util/path');
+// const rootdir = require('./util/path');
 
 const app = express();
 
-app.use(bodyparser.urlencoded({extended : false}));
+app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
-const admin = require('./routes/admin');
+app.use(bodyparser.urlencoded({extended : false}));
+ 
+const adminData = require('./routes/admin');
 const shop = require('./routes/shop');
 
-app.use('/admin', admin);
+app.use('/admin', adminData.routes);
 app.use(shop);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
-    res.sendFile(path.join(rootdir,'views','error.html'))
+    //nres.sendFile(path.join(rootdir,'views','error.html'))
+    res.render('error', { pageTitle : 'page not found'});
 });
 
 
-app.listen(3000);
+app.listen(3005);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,17 @@
 const express = require ('express');
 const path = require ('path');
 const rootdir = require ("../util/path");
-
+const adminData = require("./admin")
 
 const router = express.Router();
+// const products = require ('./admin')
 
 router.get('/',(req , res, next) => {
-    
-    res.sendFile(path.join(rootdir, 'views','shop.html'))
-    
+    console.log('shop.js' ,adminData.products);
+    res.render('shop',{
+        pageTitle : 'shops',
+        prods : adminData.products
+    });
 });
 module.exports= router;
 

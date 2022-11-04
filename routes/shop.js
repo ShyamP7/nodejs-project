d24479651c78679545f8d4616332d4938d1e2878
shyam <shyamprasadp907@gmail.com>
2022-11-04 10:59:13 +0530

    mvc added

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,11 @@
-const express = require ('express');
-const path = require ('path');
-const rootdir = require ("../util/path");
-const adminData = require("./admin")
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
-// const products = require ('./admin')
 
-router.get('/',(req , res, next) => {
-    console.log('shop.js' ,adminData.products);
-    res.render('shop',{
-        pageTitle : 'shops',
-        prods : adminData.products
-    });
-});
-module.exports= router;
+router.get('/', productsController.getProducts);
 
+module.exports = router;
